f4ad44944085129f0ead8260e25c93e795704f72
Dominic Scimeca <dominic@sustainableliving.com>
2023-10-06 16:55:15 -0700

    React Component Test

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import {Profile} from "./core/profile.tsx";
+import {settingsPersistence} from "./core/registry.ts";
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +11,7 @@ function App() {
   return (
     <>
       <div>
+          <Profile settingsPersistence={settingsPersistence}/>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
--- src/core/mock-registry.ts
+++ src/core/mock-registry.ts
@@ -1,4 +1,6 @@
 import {IndexedDBDatabase} from "./IndexDB.service.ts";
 import { mockDeep } from 'vitest-mock-extended';
+import {SettingsPersistence} from "./settings.persistence.ts";
 
 export const indexedDBDatabase  = mockDeep<IndexedDBDatabase>()
+export const settingsPersistence = mockDeep<SettingsPersistence>()
